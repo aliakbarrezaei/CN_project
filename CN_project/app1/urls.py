0f12a57972cc88226702e7bc1bcf28c2a008bd2d
ali akbar rezaei <aliakbarrezaei98@gmail.com>
2022-08-01 15:49:54 +0430

    update

--- CN_project/app1/urls.py
+++ CN_project/app1/urls.py
@@ -1,11 +1,14 @@
 
-from django.urls import path,re_path
+from django.urls import path
 from . import views
 app_name='app1'
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
-    #path('login/', views.user_login, name='login'),
-    #path('logout/', views.user_logout, name='logout'),
-    #path('upload/', views.upload_video, name='upload'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('upload/', views.upload_video, name='upload'),
+    path('video/add_comment/', views.add_comment, name='add_comment'),
+    path('video/add_like/<int:video_id>/', views.add_like, name='add_like'),
+    path('video/add_dislike/<int:video_id>/', views.add_dislike, name='add_dislike'),
 ]
