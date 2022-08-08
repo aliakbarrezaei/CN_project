dcf08bebd0076781f57c35c8883490d785497fc3
Sara <58365375+dimsra@users.noreply.github.com>
2022-08-08 11:28:18 +0430

    update (watch video + login)
    added profile
    updated video streaming (html + socket)
    updated like, dislike, comment functions
    updated login

--- CN_project/app1/views.py
+++ CN_project/app1/views.py
@@ -1,10 +1,14 @@
-from django.contrib.auth.models import User
+import os, mimetypes
+from wsgiref.util import FileWrapper
 from django.http import HttpResponse, JsonResponse
+from django.http.response import StreamingHttpResponse
+from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from . import models
-import pathlib
+from .client import Client
 
 
 @csrf_exempt
@@ -21,23 +25,16 @@ def home(request):
 
 
 @csrf_exempt
-def upload_video(request):
-    try:
-        if request.method == 'POST':
-            title = request.POST['title']
-            video_file = request.FILES['video_file']
-            user_obj = models.Users.objects.get(user__username=request.user)
-            if user_obj.status == 'N':
-                upload_video = models.Video(user=user_obj, title=title, video_file=video_file)
-                upload_video.save()
-                return HttpResponse('The video has been uploaded.')
-            else:
-                return HttpResponse('you are striked!!')
+@login_required
+def profile(request):
+    user = request.user
+    username = user.username
+    is_staff = user.is_staff
+    resp = f'username = {username}<br>is_staff = {is_staff}'
+    return HttpResponse(resp)
 
-    except:
-        return HttpResponse('error')
-
-
+ 
+# ------------------------------------------  management -------------------------------------
 @csrf_exempt
 def signup(request):
     if request.method == 'POST':
@@ -91,15 +88,22 @@ def user_login(request):
                     user = models.Users.objects.get(user=check_user)
                     if user.status == 'N':
                         login(request, check_user)
-                        return HttpResponse(f'hi {username}!')
+                        return redirect('app1:profile')
                     else:
                         login(request, check_user)
-                        return HttpResponse(f'hi {username}!, you are striked.so you cant upload')
+                        return redirect('app1:profile')
+                        # return HttpResponse(f'hi {username}!, you are striked. so you cant upload')
                 except:
-                    return HttpResponse(f'invalid username.')
+                    return render(request, 'login.html', {'is_staff': False,
+                                                          'error_message': 'Incorrect username and / or password.'})
             else:
-                return HttpResponse(f'invalid username.')
-    return HttpResponse(f'you need to log out first')
+                return render(request, 'login.html', {'is_staff': False,
+                                                      'error_message': 'Incorrect username and / or password.'})
+                # return HttpResponse(f'invalid username.')
+        else:
+            return render(request, 'login.html', {'is_staff': False})
+    else:
+        return HttpResponse(f'you need to log out first')
 
 
 @csrf_exempt
@@ -114,14 +118,20 @@ def admin_login(request):
                     admin = models.Admin.objects.get(admin=check_user)
                     if admin.status == 'C':
                         login(request, check_user)
-                        return HttpResponse(f'hi {username}!')
+                        return redirect('app1:profile')
                     else:
-                        return HttpResponse(f'hi {username}!,Your registration has not been confirmed')
+                        return render(request, 'login.html', {'is_staff': True,
+                                                              'error_message': 'Your registration has not been confirmed'})
                 except:
-                    return HttpResponse(f'invalid username.')
+                    return render(request, 'login.html', {'is_staff': True,
+                                                          'error_message': 'Incorrect username and / or password.'})
             else:
-                return HttpResponse(f'invalid username.')
-    return HttpResponse(f'you need to log out first')
+                return render(request, 'login.html', {'is_staff': False,
+                                                      'error_message': 'Incorrect username and / or password.'})
+        else:
+            return render(request, 'login.html', {'is_staff': True})
+    else:
+        return HttpResponse(f'you need to log out first')
 
 
 @csrf_exempt
@@ -132,61 +142,38 @@ def user_logout(request):
 
 
 @csrf_exempt
-def add_comment(request):
+def strike_resolving(request, username):
     try:
-        if request.method == 'POST':
-            video_id = request.POST['video_id']
-            comment = request.POST['comment']
-            video_obj = models.Video.objects.get(id=video_id)
-            user_obj = models.Users.objects.get(user__username=request.user)
-            create_comment = models.Comment.objects.create(video=video_obj, user=user_obj, comment=comment)
-            create_comment.save()
-            return HttpResponse('your comment sent')
+        admin_obj = models.Admin.objects.get(admin__username=request.user)
+        user_obj = models.Users.objects.get(user__username=username)
+        if user_obj.status == 'S':
+            user_obj.status = 'N'
+            user_obj.save()
+            return HttpResponse('The user removed from the strike mode')
+        else:
+            return HttpResponse('The user was not strike')
     except:
         return HttpResponse('error')
 
 
+# ------------------------------------------  videos -------------------------------------
 @csrf_exempt
-def add_like(request):
+def upload_video(request):
     try:
         if request.method == 'POST':
-            video_id = request.POST['video_id']
+            title = request.POST['title']
+            video_file = request.FILES['video_file']
             user_obj = models.Users.objects.get(user__username=request.user)
-            video_obj = models.Video.objects.get(id=video_id)
-            if user_obj in video_obj.dislikes.all():
-                video_obj.dislikes.remove(user_obj)
-                video_obj.likes.add(user_obj)
-                return HttpResponse('your like add')
-            elif user_obj in video_obj.likes.all():
-                video_obj.likes.remove(user_obj)
-                return HttpResponse('your like remove')
+            if user_obj.status == 'N':
+                upload_video = models.Video(user=user_obj, title=title, video_file=video_file)
+                upload_video.save()
+                return HttpResponse('The video has been uploaded.')
             else:
-                video_obj.likes.add(user_obj)
-                return HttpResponse('your like add')
+                return HttpResponse('you are striked!!')
+
     except:
         return HttpResponse('error')
-
-
-@csrf_exempt
-def add_dislike(request):
-    try:
-        if request.method == 'POST':
-            video_id = request.POST['video_id']
-            user_obj = models.Users.objects.get(user__username=request.user)
-            video_obj = models.Video.objects.get(id=video_id)
-            if user_obj in video_obj.likes.all():
-                video_obj.likes.remove(user_obj)
-                video_obj.dislikes.add(user_obj)
-                return HttpResponse('your dislike add')
-            elif user_obj in video_obj.dislikes.all():
-                video_obj.likes.remove(user_obj)
-                return HttpResponse('your dislike remove')
-            else:
-                video_obj.dislikes.add(user_obj)
-                return HttpResponse('your dislike add')
-    except:
-        return HttpResponse('error')
-
+    
 
 @csrf_exempt
 def add_label(request):
@@ -230,6 +217,130 @@ def video_status(request):
                 return HttpResponse('The video was unavailable')
     except:
         return HttpResponse('error')
+    
+
+@csrf_exempt
+@login_required
+def stream_video(request, video_id):
+    try:
+        video_obj = models.Video.objects.get(id=video_id)
+        if video_obj.status == 'I':
+            return HttpResponse('video is unavailable')
+        comments = models.Comment.objects.filter(video=video_obj)
+        user_vote = ''
+        if not request.user.is_staff:
+            user_obj = models.Users.objects.get(user__username=request.user)
+            user_vote = models.user_vote(user_obj, video_obj)
+        return render(request, 'video_view.html', {'video_url': f'f/',
+                                                   'video': video_obj,
+                                                   'video_id': video_id,
+                                                   'comments': comments,
+                                                   'user_vote': user_vote})
+    except models.Video.DoesNotExist:
+        return HttpResponse('invalid video ID')
+
+
+@login_required
+@csrf_exempt
+def fstream(request, video_id):
+    video_obj = models.Video.objects.get(id=video_id)
+    if video_obj.status == 'I':
+        return HttpResponse('video is unavailable')
+    path = video_obj.get_path()
+    size = os.path.getsize(path)
+    range_header = request.META.get('HTTP_RANGE', '').strip()
+    range_match = models.range_re.match(range_header)
+    content_type, encoding = mimetypes.guess_type(path)
+    file = open(path, 'rb')
+    if range_match:
+        start, end = range_match.groups()
+        start = int(start)
+        end = int(end) if end else size - 1
+        if end >= size:
+            end = size - 1
+        length = end - start + 1
+
+        bytes = models.RangeFileWrapper(file, offset=start, length=length)
+        resp = StreamingHttpResponse(bytes, status=206, content_type=content_type)
+        resp['Content-Range'] = 'bytes %s-%s/%s' % (start, end, size)
+        resp['Content-Length'] = str(length)
+    else:
+        bytes = FileWrapper(file)
+        resp = StreamingHttpResponse(bytes, content_type=content_type)
+        resp['Content-Length'] = str(size)
+        resp['Cache-Control'] = 'no-cache'
+    resp['Accept-Ranges'] = 'bytes'
+    return resp
+
+
+@csrf_exempt
+def add_comment(request, video_id):
+    try:
+        if request.method == 'POST':
+            video_id = video_id
+            comment = request.POST['comment']
+            video_obj = models.Video.objects.get(id=video_id)
+            user_obj = models.Users.objects.get(user__username=request.user)
+            if not len(comment.split()):
+                return JsonResponse({'update_error': {'msg': 'you cant post an empty comment!'}})
+            create_comment = models.Comment.objects.create(video=video_obj, user=user_obj, comment=comment)
+            create_comment.save()
+            resp = {'cmn_username': user_obj.user.username, 'cmn_text': comment}
+            return JsonResponse({'update_data': resp})
+    except models.Users.DoesNotExist:
+        return JsonResponse({'update_error': {'msg': 'you cant post comment!'}})
+    except models.Video.DoesNotExist:
+        return JsonResponse({'update_error': {'msg': 'invalid video ID'}})
+
+
+@csrf_exempt
+def add_like(request, video_id):
+    try:
+        '''if request.method == 'POST':
+            video_id = request.POST['video_id']'''
+        user_obj = models.Users.objects.get(user__username=request.user)
+        video_obj = models.Video.objects.get(id=video_id)
+        if user_obj in video_obj.dislikes.all():
+            video_obj.dislikes.remove(user_obj)
+            video_obj.likes.add(user_obj)
+        elif user_obj in video_obj.likes.all():
+            video_obj.likes.remove(user_obj)
+        else:
+            video_obj.likes.add(user_obj)
+        user_vote = models.user_vote(user_obj, video_obj)
+        resp = {'likes_count': video_obj.like_count(),
+                'dislikes_count': video_obj.dislike_count(),
+                'user_vote': user_vote}
+        return JsonResponse({'update_data': resp})
+    except models.Users.DoesNotExist:
+        return JsonResponse({'update_error': {'msg': 'you cant vote!'}})
+
+    except models.Video.DoesNotExist:
+        return JsonResponse({'update_error': {'msg': 'invalid video ID'}})
+
+
+@csrf_exempt
+def add_dislike(request, video_id):
+    try:
+        user_obj = models.Users.objects.get(user__username=request.user)
+        video_obj = models.Video.objects.get(id=video_id)
+        if user_obj in video_obj.likes.all():
+            video_obj.likes.remove(user_obj)
+            video_obj.dislikes.add(user_obj)
+        elif user_obj in video_obj.dislikes.all():
+            video_obj.dislikes.remove(user_obj)
+        else:
+            video_obj.dislikes.add(user_obj)
+        user_vote = models.user_vote(user_obj, video_obj)
+        resp = {'likes_count': video_obj.like_count(),
+                'dislikes_count': video_obj.dislike_count(),
+                'user_vote': user_vote}
+        return JsonResponse({'update_data': resp})
+    except models.Users.DoesNotExist:
+        return JsonResponse({'update_error': {'msg': 'you cant vote!'}})
+
+    except models.Video.DoesNotExist:
+        return JsonResponse({'update_error': {'msg': 'invalid video ID'}})
 
 
 @csrf_exempt
@@ -238,7 +349,7 @@ def watch_video(request, video_id):
     try:
         video_obj = models.Video.objects.get(id=video_id)
         if video_obj.video_status != 'I':
-            comments = models.Comment.objects.filter(video=video_obj).order_by('-id')
+            comments = models.Comment.objects.filter(video=video_obj)
             video_comments = [{'user': comment.user.user.username, 'comment': comment.comment} for comment in comments]
             video_likes = video_obj.likes.all().count()
             video_dislikes = video_obj.dislikes.all().count()
@@ -248,35 +359,15 @@ def watch_video(request, video_id):
                        'label': video_obj.label,
                        'likes': video_likes,
                        'dislikes': video_dislikes,
-                       'comments': video_comments }
-            base = str(pathlib.Path(__file__).parent.resolve()).replace('\\', '/').replace('/app1', '')
-            video_path = base + '/media/' + str(video_obj.video_file)
-            # print(video_path)
-            client = models.Client('127.0.0.1', 8001, video_path)
+                       'comments': video_comments}
+
+            user = request.user
+            password = request.POST['password']
+            client = Client('127.0.0.1', 8001, video_id, user.username, password)
             return JsonResponse({'details': details})
 
         else:
             return HttpResponse(f'video is inaccessible')
     except models.Video.DoesNotExist:
         return HttpResponse(f'There is no video with id={video_id}')
-
-
-@csrf_exempt
-def strike_resolving(request, username):
-    try:
-        admin_obj = models.Admin.objects.get(admin__username=request.user)
-        user_obj = models.Users.objects.get(user__username=username)
-        if user_obj.status == 'S':
-            user_obj.status = 'N'
-            user_obj.save()
-            return HttpResponse('The user removed from the strike mode')
-        else:
-            return HttpResponse('The user was not strike')
-    except:
-        return HttpResponse('error')
-
-      
-
-
-
 
