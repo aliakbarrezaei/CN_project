870f6626edda5d5bf9cef35e547e1c6cdb8be64f
Sara <58365375+dimsra@users.noreply.github.com>
2022-08-08 11:12:45 +0430

    updated Video and Comment display

--- CN_project/app1/admin.py
+++ CN_project/app1/admin.py
@@ -1,13 +1,27 @@
 from django.contrib import admin
 from . import models
-# Register your models here.
+
+
 @admin.register(models.Users)
 class UserManager(admin.ModelAdmin):
-    list_display=('user','status')
-    list_filter=('status',)
+    list_display = ('user', 'status')
+    list_filter = ('status',)
+
+
 @admin.register(models.Admin)
 class AdminManager(admin.ModelAdmin):
-    list_display=('admin','status')
-    list_filter=('status',)
-admin.site.register(models.Video)
-admin.site.register(models.Comment)
+    list_display = ('admin', 'status')
+    list_filter = ('status',)
+
+
+@admin.register(models.Video)
+class AdminManager(admin.ModelAdmin):
+    list_display = ('id', 'title', 'user', 'pub_date', 'status', 'label', 'video_file')
+    list_filter = ('id', 'title', 'user', 'pub_date', 'status', 'label')
+
+
+@admin.register(models.Comment)
+class AdminManager(admin.ModelAdmin):
+    list_display = ('id', 'user', 'comment', 'video')
+    list_filter = ('id', 'user', 'comment', 'video')
+
